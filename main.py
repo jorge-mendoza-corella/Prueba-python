4a7482cbc28caa5cbbe349b8e4f2301a3f1242d8
Jorge Mendoza <mencor.corp@gmail.com>
2022-12-29 21:30:59 -0600

    el del dia de hoy

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 print("hola")
 
+print(__name__)
+
 var1 = input("dame este dato ")
 
 #es para pedir datos
 print(f"Este es el valor {var1}")
+var1 = bool(var1)
+print(type(var1))
 
-print("vamos a hacer algo")
+if type(var1) == str:
+    print("es un string")
+elif type(var1) == int:
+    print("es un numero")
+else:
+     print("es otra cosa")
