ac14031877e7f96484b6889318f596139901e96e
Jorge Mendoza <mencor.corp@gmail.com>
2023-01-16 21:26:04 -0600

    .gitignore is now working

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,18 @@
-def poblacion_por_pais(data,pais):
+def poblacion_por_pais(data, pais):
     result = list(filter(lambda item: item['Country'] == pais, data))
     return result
 
 
 def get_population(pais):
     poblacion = {
-        '2022' : int(pais['2022 Population']),
-        '2020' : int(pais['2020 Population']),
-        '2015' : int(pais['2015 Population']),
-        '2010' : int(pais['2010 Population']),
-        '2000' : int(pais['2000 Population']),
-        '1990' : int(pais['1990 Population']),
-        '1980' : int(pais['1980 Population']),
-        '1970' : int(pais['1970 Population'])
+        '2022': int(pais['2022 Population']),
+        '2020': int(pais['2020 Population']),
+        '2015': int(pais['2015 Population']),
+        '2010': int(pais['2010 Population']),
+        '2000': int(pais['2000 Population']),
+        '1990': int(pais['1990 Population']),
+        '1980': int(pais['1980 Population']),
+        '1970': int(pais['1970 Population'])
     }
 
     print((poblacion))
@@ -21,8 +21,9 @@ def get_population(pais):
     return labels, values
 
 
-def world_percentages(data): 
+def world_percentages(data):
     pais = list(map(lambda pais: pais['Country'], data))
-    porcentajes = list(map(lambda porcentaje: porcentaje['World Population Percentage'], data))
-    #datos = zip (pais,porcentajes)
+    porcentajes = list(
+        map(lambda porcentaje: porcentaje['World Population Percentage'], data))
+    # datos = zip (pais,porcentajes)
     return pais, porcentajes
